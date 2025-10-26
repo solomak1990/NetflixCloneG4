446d75698ff814496e254080dd3324cc4e246780
Solomon <36040843+solomak1990@users.noreply.github.com>
2025-10-26 22:07:42 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/NetflixCloneG4/",
   plugins: [react()],
-});
-// export default defineConfig({
-//   base: "/NetflixCloneG4/",
-//   plugins: [react()],
-// });
-
+  base: '/NetflixCloneG4/'   // <--- add this
+})
