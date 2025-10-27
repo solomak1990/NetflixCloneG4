a20ccc2d7e26c1cbe19818c8f4e215b8bf6b8c21
Solomon Hunegnaw <hunegnaws@gmail.com>
2025-10-27 08:51:02 +0300

    Resolve merge conflicts in package.json and package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/netflixClone/", // 👈 must match your repo name
+  // base: "/netflixClone/",
   base: "/", 
 });
